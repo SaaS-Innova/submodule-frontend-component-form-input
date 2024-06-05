af8a801df8f90e95ca5825c9e5baf5a445017529
Sakshi Antala <sakshi@saasinnova.com>
2024-06-05 14:23:33 +0530

    Code Optimization

--- Input.tsx
+++ Input.tsx
@@ -1,70 +1,83 @@
 import { InputText } from "primereact/inputtext";
-import { useFormContext } from "react-hook-form";
-import { IFormFieldType, IFormProps } from "../forms.model";
-import { FormFieldError } from "../../FormFieldError/FormFieldError";
-import { inputValidator } from '../../../library/utilities/helperFunction';
-import { useEffect } from "react";
+import { Controller, useFormContext } from "react-hook-form";
+import { inputValidator } from "../../../../library/utilities/helperFunction";
+import { FormFieldError } from "../FormFieldError/FormFieldError";
+import { IFormProps } from "../FormInterface/forms.model";
+import { IFormFieldType } from "../../../../library/utilities/constant";
 
 export const Input = (props: IFormProps) => {
   const { attribute, form, fieldType } = props;
-  const { initialValue, label } = form[attribute];
-  const { required, maxLength, type } = form[attribute].rules;
+  const { label } = form[attribute];
+  const { required, maxLength, type, disabled, placeholder } =
+    form[attribute].rules;
   const {
     register,
-    setValue,
+    control,
     formState: { errors },
   } = useFormContext();
-  let labelClassName = "";
-  let fieldClassName = "";
-  let divClassName = "";
 
-  switch (fieldType) {
-    case IFormFieldType.NO_LABEL:
-      labelClassName = ""; 
-      fieldClassName = "field p-fluid";
-      divClassName = "";
-      break;
-    case IFormFieldType.TOP_LABEL:
-      labelClassName = "";
-      fieldClassName = "field p-fluid";
-      divClassName = "";
-      break;
-    default:
-      labelClassName = "col-12 mb-3 md:col-3 md:mb-0";
-      fieldClassName = "field grid";
-      divClassName = "col-12 md:col-9 relative";
-      break;
-  }
+  const getClassNames = () => {
+    let labelClassName = "";
+    let fieldClassName = "";
+    let divClassName = "";
+
+    switch (fieldType) {
+      case IFormFieldType.NO_LABEL:
+        labelClassName = "";
+        fieldClassName = "field p-fluid";
+        divClassName = "";
+        break;
+      case IFormFieldType.TOP_LABEL:
+        labelClassName = "";
+        fieldClassName = "field p-fluid";
+        divClassName = "";
+        break;
+      default:
+        labelClassName = "col-12 mb-3 md:col-3 md:mb-0";
+        fieldClassName = "field grid";
+        divClassName = "col-12 md:col-9 relative";
+        break;
+    }
+
+    return { labelClassName, fieldClassName, divClassName };
+  };
+  const { labelClassName, fieldClassName, divClassName } = getClassNames();
   const labelElement = (
     <label htmlFor={attribute} className={labelClassName}>
       {label} {required && "*"}
     </label>
   );
-  useEffect(() => {
-    setValue(attribute, initialValue);
-  }, [initialValue]);
+
   return (
     <div className={fieldClassName}>
-      {fieldType !== "no-label" && labelElement}
+      {fieldType !== IFormFieldType.NO_LABEL && labelElement}
       <div className={divClassName}>
         <div className="p-inputgroup">
-          <InputText
-            style={{ width: "100%" }}
-            type={type ?? "text"}
-            id={attribute}
-            {...register(attribute, {
-              ...inputValidator(form[attribute].rules, label),
-              value: initialValue,
-            })}
-            maxLength={maxLength}
-            placeholder={form[attribute].placeholder}
+          <Controller
+            control={control}
+            name={attribute}
+            rules={inputValidator(form[attribute].rules, label)}
+            render={({ field }) => {
+              return (
+                <InputText
+                  {...field}
+                  value={field.value}
+                  type={type ?? "text"}
+                  id={attribute}
+                  {...register(attribute, {
+                    ...inputValidator(form[attribute].rules, label),
+                  })}
+                  autoFocus
+                  className={errors[attribute] ? "p-invalid" : ""}
+                  maxLength={maxLength}
+                  placeholder={placeholder}
+                  disabled={disabled}
+                />
+              );
+            }}
           />
         </div>
-        <div className="pb-2 pt-1">
-          <FormFieldError
-            data={{ errors, name: attribute,className: 'absolute' }}
-          />
-        </div>
+        <FormFieldError data={{ errors, name: attribute }} />
       </div>
     </div>
   );
